9a57ea73ca9f23fb99e71cb9dd7d2a46f001e986
Moner <69835956+2monerDs@users.noreply.github.com>
2021-01-08 22:26:34 -0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,54 @@
+config
+
+const   Discord   =   require  ( "discord.js" ) ;
+const   dontenv   =   require  ( "dotenv" ) ;
+const   fs   =   require  ( "fs" ) ;
+const   path   =   require  ( "path" ) ;
+
+dontenv . config  ( ) ;
+
+const   bot   =   novo   Discord . Cliente  ( ) ;
+bot . comandos   =   novo   Discord . Coleção  ( ) ;
+bot . filas   =   novo   Mapa  ( ) ;
+
+const   commandFiles   =   fs . readdirSync  ( path . join   ( __dirname ,   "/ comandos" ) )
+. filter  ( ( nome  do  arquivo )   =>   nome  do  arquivo . endsWith  ( ". js" ) ) ;
+
+para  ( var   nome   do   arquivo  de  commandFiles )  {
+   comando   const  =   requer  ( `. / comandos / $ {nome do arquivo}` ) ;
+  bot . comandos . set  ( comando . nome ,   comando ) ;
+}
+
+bot . login  ( processo . env . TOKEN ) ;
+
+bot . em  ( "pronto" ,   ( )   =>   {
+  deixe   atividades   =  [
+      `Utilize $ {process.env.PREFIX} ajuda para obter ajuda!` ,
+      `$ {bot.guilds.cache.size} servidores!` ,
+      `O Moner é meu Dono!` ,
+      `Rusty Apex!`
+    ]  ,
+    i   =   0 ;
+  setInterval  (   ( )   =>   bot . user . setActivity  ( `$ {Activities [i ++% Activities.length]}` ,   {
+        digite :   "STREAMING" ,   url :   'https: // www.twitch.tv / moner_gsd'
+      } ) ,   1000   *   60 ) ;    // ASSISTIR, ESCUTAR, JOGAR, TRANSMITIR
+
+  bot . do utilizador
+      . setStatus  ( "dnd" )  // idle, dnd, online, invisível
+      . catch  ( console . erro ) ;
+console . log  ( "Estou Online!" )
+} ) ;
+
+
+bot . em  ( "mensagem" ,   ( msg )   =>   {
+  if   ( ! msg . content . startsWith  ( process . env . PREFIX )   ||   msg . author . bot )   return ;
+
+  const   args   =   msg . conteúdo . fatia  ( processo . env . PREFIXO . comprimento )  . dividir  ( "" ) ; 
+   comando   const  =   args . shift  ( ) ;
+
+  tente  {
+    bot . comandos . get  ( comando )  . executar  ( bot ,   msg ,   args ) ;
+    }   pegar   ( e )  {
+    return   msg . responder  ( "Ops! Eu ainda não tenho esse comando." ) ;
+  }
+} ) ;
